d9b053ab56ef2259529f17c268216ade938d3462
Arunas Bendoraitis <arunas.bendoraitis@teltonika.lt>
2020-09-21 09:10:18 +0300

    Reorder statements

--- src/emqx_dynamodb_hooks.erl
+++ src/emqx_dynamodb_hooks.erl
@@ -1,33 +1,33 @@
 -module(emqx_dynamodb_hooks).
 
+%% See 'Application Message' in MQTT Version 5.0
+-record(message, {
+    %% Global unique message ID
+    id :: binary(),
+    %% Message QoS
+    qos = 0,
+    %% Message from
+    from :: atom() | binary(),
+    %% Message flags
+    flags = #{} :: emqx_types:flags(),
+    %% Message headers. May contain any metadata. e.g. the
+    %% protocol version number, username, peerhost or
+    %% the PUBLISH properties (MQTT 5.0).
+    headers = #{} :: emqx_types:headers(),
+    %% Topic that the message is published to
+    topic :: emqx_types:topic(),
+    %% Message Payload
+    payload :: emqx_types:payload(),
+    %% Timestamp (Unit: millisecond)
+    timestamp :: integer()
+    }).
+
 -include_lib("emqx/include/emqx.hrl").
 
 -export([load/1, unload/0]).
 
 %% Message Pubsub Hooks
 -export([on_message_publish/2]).
-
-% %% See 'Application Message' in MQTT Version 5.0
-% -record(message, {
-%     %% Global unique message ID
-%     id :: binary(),
-%     %% Message QoS
-%     qos = 0,
-%     %% Message from
-%     from :: atom() | binary(),
-%     %% Message flags
-%     flags = #{} :: emqx_types:flags(),
-%     %% Message headers. May contain any metadata. e.g. the
-%     %% protocol version number, username, peerhost or
-%     %% the PUBLISH properties (MQTT 5.0).
-%     headers = #{} :: emqx_types:headers(),
-%     %% Topic that the message is published to
-%     topic :: emqx_types:topic(),
-%     %% Message Payload
-%     payload :: emqx_types:payload(),
-%     %% Timestamp (Unit: millisecond)
-%     timestamp :: integer()
-%     }).
 
 %% Called when the plugin application start
 load(Env) ->
@@ -42,9 +42,18 @@ on_message_publish(Message = #message{topic = <<"$SYS/", _/binary>>}, _Env) ->
     {ok, Message};
 on_message_publish(Message = #message{payload = Payload}, _Env) ->
     TableName = application:get_env(emqx_dynamodb, table_name, <<"">>),
-    erlcloud:put_item(TableName, Payload),
     io:format("Publish ~s~n", [format(Message)]),
+    erlcloud_ddb2:put_item(TableName, Payload),
     {ok, Message}.
+
+% mapPayloadToDDB(Payload) ->
+%     [
+%         {<<"client_id">>},
+%         {<<"timestamp">>},
+%         {<<"latitude">>},
+%         {<<"longitude">>},
+%         {<<"altitude">>}
+%     ].
 
 format(#message{id = Id, qos = QoS, topic = Topic, from = From, payload = Payload}) ->
     io_lib:format("Message(Id=~s, QoS=~w, Topic=~s, From=~p, Payload=~s)",
@@ -53,3 +62,5 @@ format(#message{id = Id, qos = QoS, topic = Topic, from = From, payload = Payloa
 %% Called when the plugin application stop
 unload() ->
     emqx:unhook('message.publish', {?MODULE, on_message_publish}).
+
+
